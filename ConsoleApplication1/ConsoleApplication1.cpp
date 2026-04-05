ecc4b18ea16c5126fb1532c80990561b40d15c21
user <user@WIN-GLNPNI3IG4L>
2026-04-05 20:07:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double x, y;
+    cout << "Введите x y:";
+    cin >> x >> y;
+
+    if (x * x + y * y < 100 && y > abs(x))
+        cout << "ответ: Да";
+    else if ((x * x + y * y == 100 && y >= abs(x)) ||
+        (x * x + y * y <= 100 && y == abs(x)))
+        cout << "ответ: На границе";
+    else
+        cout << "ответ: Нет";
+
+    return 0;
+}
+
