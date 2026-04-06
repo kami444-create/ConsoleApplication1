78d0eec0ac5e05c49a7cc9e29ae51dea2fdbf00e
kami444-create <kamiduro444@gmail.com>
2026-04-06 12:28:04 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,21 +1,22 @@
-﻿#include <iostream>
-#include <cmath>
+#include <iostream> // Ввод/вывод
+#include <cmath> // abs()
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    double x, y;
-    cout << "Введите x y:";
-    cin >> x >> y;
+int main() 
+{
+    setlocale(LC_ALL, "Russian"); // Русская локализация
+    double x, y;// Координаты точки
+    
+    cout << "Введите x y:"; // Приглашение к вводу
+    cin >> x >> y; // Считываем координаты
 
-    if (x * x + y * y < 100 && y > abs(x))
+    if (x*x + y*y < 100 && y > abs(x)) // Внутри круга и выше y=|x|
         cout << "ответ: Да";
-    else if ((x * x + y * y == 100 && y >= abs(x)) ||
-        (x * x + y * y <= 100 && y == abs(x)))
+    else if ((x*x + y*y == 100 && y >= abs(x)) || // На окружности и выше y=|x|
+             (x*x + y*y <= 100 && y == abs(x))) // Внутри круга на y=|x|
         cout << "ответ: На границе";
     else
-        cout << "ответ: Нет";
+        cout << "ответ: Нет"; // Вне области
 
-    return 0;
+    return 0;  // Завершение программы
 }
-
